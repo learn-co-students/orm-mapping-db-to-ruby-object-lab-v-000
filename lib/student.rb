6e56ede65ad5319007b59f70bbc25c3edac65333
jakebrady5 <jakebrady5@gmail.com>
2015-12-04 23:16:55 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,16 +3,61 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    newbie = Student.new
+    newbie.id = row[0]
+    newbie.name = row[1]
+    newbie.grade = row[2]
+    newbie
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    query = <<-SQL
+      SELECT * FROM students;
+      SQL
+    DB[:conn].execute(query).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    query = <<-SQL
+      SELECT * FROM students
+      WHERE name = ?;
+      SQL
+    DB[:conn].execute(query, name).map do |row|
+      newbie = Student.new_from_db(row)
+    end.first
+  end
+
+  def self.students_below_12th_grade
+    query = <<-SQL
+      SELECT * FROM students
+      WHERE grade < ?;
+      SQL
+    DB[:conn].execute(query, 12).map
+  end
+
+  def self.first_student_in_grade_10
+    query = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+      LIMIT 1;
+      SQL
+    result = DB[:conn].execute(query, 10)
+    self.find_by_name(result[0][1])
+  end
+
+  def self.count_all_students_in_grade_9
+    query = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?;
+      SQL
+    DB[:conn].execute(query, 9)
+    
   end
   
   def save
