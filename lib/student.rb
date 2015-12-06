dd33988ff627dbb085f0ef25462bb32b895a7586
frankNowinski <nowinski.frank@gmail.com>
2015-12-05 21:10:15 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,36 +1,57 @@
+require "pry"
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = self.new
+    student.id = row[0] 
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
-  def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+  def self.all 
+    sql = "SELECT * FROM students"
+    DB[:conn].execute(sql).map{ |row| new_from_db(row) }
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = "SELECT * FROM students WHERE name = ?"
+    DB[:conn].execute(sql, name).map{ |row| new_from_db(row) }.first
   end
-  
+
+  def self.students_below_12th_grade
+    sql = "SELECT * FROM students WHERE grade < 12"
+    DB[:conn].execute(sql).map{ |row| new_from_db(row) }
+  end
+
+  def self.first_student_in_grade_10
+    sql = "SELECT * FROM students WHERE grade = 10"
+    DB[:conn].execute(sql).map{ |row| new_from_db(row) }.first
+  end
+
+  def self.count_all_students_in_grade_9
+    sql = "SELECT COUNT(*) FROM students WHERE grade = 9"
+    DB[:conn].execute(sql).map{ |row| new_from_db(row) }
+  end
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
-      VALUES (?, ?)
+      INSERT INTO students (name, grade)
+      VALUES (?,?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
   
   def self.create_table
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade TEXT
-    )
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT,
+        grade TEXT
+      )
     SQL
 
     DB[:conn].execute(sql)
