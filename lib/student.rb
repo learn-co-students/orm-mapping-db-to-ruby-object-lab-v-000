1e79d6d3a942cbf672960bbab3c521158581826a
authorbeard <msnolan@gmail.com>
2016-02-04 13:15:40 -0500

    all tests passing

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,48 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+# binding.pry
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    DB[:conn].execute("SELECT * FROM students").map{|row| self.new_from_db(row)}
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+          SELECT * FROM students
+          WHERE name = ?
+          SQL
+    self.new_from_db(DB[:conn].execute(sql, name)[0])
+  end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+          SELECT * FROM students
+          WHERE grade = 9
+          SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+          SELECT * FROM students
+          WHERE grade < 12
+          SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.first_student_in_grade_10
+    self.new_from_db(DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT 1")[0])    
   end
   
   def save
