b22b86e02901fdf2a31c5fb12d23f1383cae72db
davdkm <davd.km@gmail.com>
2016-02-09 20:12:07 -0800

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,29 +1,40 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    db = DB[:conn].execute('SELECT * FROM students')
+    db.map{|row| Student.new_from_db(row.flatten)}
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    row = DB[:conn].execute("SELECT * FROM students WHERE students.name = ?", name)
+    Student.new_from_db(row.flatten)
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
@@ -39,5 +50,18 @@ class Student
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
+  end
+
+  def self.count_all_students_in_grade_9
+    DB[:conn].execute("SELECT COUNT(*) FROM students WHERE students.grade = 9")
+  end
+
+  def self.students_below_12th_grade
+    DB[:conn].execute("SELECT * FROM students WHERE students.grade < 12")
+  end
+
+  def self.first_student_in_grade_10
+    row = DB[:conn].execute("SELECT * FROM students WHERE students.grade = 10").first
+    student = Student.new_from_db(row.flatten)
   end
 end
