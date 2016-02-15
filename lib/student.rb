c48f2be805dd73003771258d3ca07c4eb16d4696
tejbans <singh.tejbans@gmail.com>
2016-02-15 13:02:04 -0800

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,17 +2,32 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student= self.new
+    new_student.id=row[0]
+    new_student.name=row[1]
+    new_student.grade=row[2]
+    new_student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    
+    sql = <<-sql
+          select * from students
+          sql
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql= <<-sql
+          select * from students
+          where name = ?
+          limit 1
+          sql
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
   def save
@@ -40,4 +55,45 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-sql
+          select * from students
+          where grade =?
+          sql
+      DB[:conn].execute(sql,"9").map do |row|
+            self.new_from_db(row)
+      end
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-sql
+          select * from students
+          where grade < ?
+          sql
+    DB[:conn].execute(sql,12).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+
+  def self.first_student_in_grade_10
+    sql = <<-sql
+          select * from students
+          where grade =?
+          limit 1
+          sql
+      DB[:conn].execute(sql,10).map do |row|
+        self.new_from_db(row)
+      end.first
+  end
+
+
+
+
+
+
+
+
+
 end
