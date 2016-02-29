3873f965ef8102c953ed567d0da9913c7cd9b06c
shinyrachel <rachel.kory@gmail.com>
2016-02-29 16:49:19 -0500

    complete

--- lib/student.rb
+++ lib/student.rb
@@ -2,17 +2,28 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = Student.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    all_rows = DB[:conn].execute("SELECT * FROM students")
+    all_rows.collect do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+    SELECT * FROM students 
+    WHERE name = ?
+    LIMIT 1
+    SQL
+    student_row = DB[:conn].execute(sql, name).first
+    self.new_from_db(student_row)
   end
   
   def save
@@ -20,7 +31,6 @@ class Student
       INSERT INTO students (name, grade) 
       VALUES (?, ?)
     SQL
-
     DB[:conn].execute(sql, self.name, self.grade)
   end
   
@@ -32,12 +42,43 @@ class Student
       grade TEXT
     )
     SQL
-
     DB[:conn].execute(sql)
   end
 
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
+  end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+    SELECT * FROM students 
+    WHERE grade = 9
+    SQL
+    ninth_grade = DB[:conn].execute(sql)
+    ninth_grade.map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade < 12
+    SQL
+    grades = DB[:conn].execute(sql)
+    grades.map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_student_in_grade_10 
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = 10
+    LIMIT 1
+    SQL
+    student = DB[:conn].execute(sql).first
+    self.new_from_db(student)
   end
 end
