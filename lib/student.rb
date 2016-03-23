7fb9026a24a4c9e7ac7acadfbcd3a7eac6f524bb
ShiLi007 <lishidaniel@gmail.com>
2016-03-23 16:14:02 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,15 +2,38 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+  new_student = self.new  
+  new_student.id = row[0]
+  new_student.name =  row[1]
+  new_student.grade = row[2]
+  new_student 
   end
 
   def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM Students
+    SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
   end
 
   def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM Students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+ 
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+
     # find the student in the database given a name
     # return a new instance of the Student class
   end
@@ -40,4 +63,50 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+
+ def self.count_all_students_in_grade_9
+    sql = <<-SQL
+    SELECT * FROM students
+      WHERE students.grade = 9
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+
+def self.students_below_12th_grade
+  sql = <<-SQL
+    SELECT * FROM students
+      WHERE students.grade < 12
+    SQL
+    DB[:conn].execute(sql)
 end
+
+def self.first_student_in_grade_10
+  sql = <<-SQL
+    SELECT * FROM students
+      WHERE students.grade = 10
+      LIMIT 1
+    SQL
+     self.new_from_db(DB[:conn].execute(sql).first)
+end
+
+
+def self.first_x_students_in_grade_10(num)
+self.new_from_db(DB[:conn].execute("SELECT * FROM students WHERE students.grade = 10 ORDER BY students.id ASC LIMIT ?", num).first)
+end
+
+def self.all_students_in_grade_X(num)
+      sql = <<-SQL
+    SELECT * FROM students
+      WHERE students.grade = numå
+    SQL
+    DB[:conn].execute(sql)
+end
+
+
+end
+
+
+
+
