5ee88951281b2941383ea0f18ba6f2c7b41058fd
aprietof <adrianprieto00@gmail.com>
2016-04-11 22:44:27 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,11 @@ class Student
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    results = DB[:conn].execute("SELECT * FROM students")
+
+    results.map do |row|
+      Student.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
@@ -23,6 +28,42 @@ class Student
     SQL
     result = DB[:conn].execute(sql, name).flatten
     Student.new_from_db(result)
+  end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT COUNT(*) FROM students WHERE grade = 9
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.first_x_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10 LIMIT ?
+    SQL
+    DB[:conn].execute(sql, x)
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10 LIMIT 1
+    SQL
+    result = DB[:conn].execute(sql).flatten
+    Student.new_from_db(result)
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql,grade)
   end
 
   def save
