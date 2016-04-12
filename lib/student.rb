7164a48fe6bf825d96351c14b82a7910d812a21a
aprietof <adrianprieto00@gmail.com>
2016-04-11 20:40:36 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,11 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    Student.new.tap do |s|
+      s.id = row[0]
+      s.name = row[1]
+      s.grade = row[2]
+    end
   end
 
   def self.all
@@ -13,17 +18,22 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ? LIMIT 1
+    SQL
+    result = DB[:conn].execute(sql, name).flatten
+    Student.new_from_db(result)
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
