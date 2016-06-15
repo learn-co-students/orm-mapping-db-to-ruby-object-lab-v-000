1c257d7decf852c4de04cc8f961a3c61fbc395a7
rnhutsko <rnhutsko@gmail.com>
2016-06-15 17:22:31 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,19 +1,99 @@
 class Student
   attr_accessor :id, :name, :grade
 
+  @@all= []
+
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = self.new  
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student  
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+      SQL
+      DB[:conn].execute(sql).map do |student|
+        self.new_from_db(student)
+      end
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+      SQL
+      DB[:conn].execute(sql, name).map do |student|
+        self.new_from_db(student)
+      end.first
   end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE Students.grade = 9
+        SQL
+
+        DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+
+    sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE Students.grade < 12
+        SQL
+
+        DB[:conn].execute(sql)
+  end
+
+  def self.first_x_students_in_grade_10(num)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE Students.grade = 10
+      LIMIT ?
+      SQL
+
+    DB[:conn].execute(sql,num)
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE Students.grade = 10
+      LIMIT 1
+      SQL
+
+    row = DB[:conn].execute(sql)[0]
+    self.new_from_db(row)
+  end
+
+
+def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE Students.grade = ?
+      SQL
+
+    DB[:conn].execute(sql,grade)
+  end
+
+
+
+
+
+
+
+
   
   def save
     sql = <<-SQL
