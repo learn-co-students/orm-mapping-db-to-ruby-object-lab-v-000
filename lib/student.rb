8a5e1c18bb219fd67bc3c4494ff227cd9148665c
Traci7822 <tracir@gmail.com>
2016-08-24 19:37:53 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -105,9 +105,9 @@ class Student
       LIMIT ?
     SQL
 
-    DB[:conn].execute(sql).map do |row|
-        self.new_from_db(row)
-    end
+    #DB[:conn].execute(sql).map do |row|
+     #   self.new_from_db(row)
+    #end
   end
 
   def self.first_student_in_grade_10
