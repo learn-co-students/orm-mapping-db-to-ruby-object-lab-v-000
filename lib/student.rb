19b2bbe05542335abec0a833ddee1fcd9e441b77
Lani1321 <lani.berry13@gmail.com>
2016-09-07 16:47:29 -0400

    add .count_all_students_in_grade_9

--- lib/student.rb
+++ lib/student.rb
@@ -38,6 +38,18 @@ class Student
     end.first
   end
 
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT COUNT(*) 
+      FROM students
+      WHERE grade = 9
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+
   
   def save
     sql = <<-SQL
