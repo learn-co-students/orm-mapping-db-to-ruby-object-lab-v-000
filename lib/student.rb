2083766ab2567793b4089ecccb6bf4f87e854d05
Lani1321 <lani.berry13@gmail.com>
2016-09-07 16:50:24 -0400

    add .students_below_12th_grade

--- lib/student.rb
+++ lib/student.rb
@@ -49,6 +49,17 @@ class Student
     end
   end
 
+  def self.students_below_12th_grade
+    sql = <<-SQL 
+      SELECT *
+      FROM students 
+      WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
 
   
   def save
