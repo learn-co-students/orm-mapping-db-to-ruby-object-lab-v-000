0b1d5abce87fd8a71d69a01a4dd5fada52d006e0
Lani1321 <lani.berry13@gmail.com>
2016-09-07 16:27:04 -0400

    add .new_from_db to create a new student object

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,12 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id    = row[0]
+    new_student.name  = row[1]
+    new_student.grade = row[2]
+    # Return the newly created instance
+    new_student
   end
 
   def self.all
@@ -40,4 +46,5 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
 end
