e6836099dbfac8d4b7bd58372d163ad9727495e9
Jason Casares <jasonc@csdrafting.com>
2016-09-18 21:45:36 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,7 @@ class Student
     new_student = self.new
     new_student.id = row[0]
     new_student.name = row[1]
+    new_student.grade = row[2]
     new_student
     # create a new Student object given a row from the database
   end
@@ -58,6 +59,52 @@ class Student
     SQL
 
     DB[:conn].execute(sql)
+  end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT COUNT(*)
+      FROM students
+      WHERE grade = 9;
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id ASC LIMIT 1
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.first_x_students_in_grade_10(num)
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
+    DB[:conn].execute(sql,num)
+  end
+
+
+
+  def self.all_students_in_grade_X(grade=10)
+    sql = "SELECT * FROM students WHERE grade = ?"
+    DB[:conn].execute(sql,grade)
   end
 
   def self.drop_table
