0b18eebc1084cbd5bd6ebe994ab525605ae91cd7
Senad Hadrovic <senadhadrovic@gmail.com>
2016-12-20 05:04:48 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,10 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+      new_student = Student.new
+      new_student.id = row[0]
+      new_student.name = row[1]
+      new_student.grade = row[2]
   end
 
   def self.all
