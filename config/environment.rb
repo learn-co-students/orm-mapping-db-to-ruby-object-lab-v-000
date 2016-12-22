d1d5c4cc67b6be828243fac03e5bbecfdadee589
Umohm1 <umohm12@gmail.com>
2016-12-22 03:20:51 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,4 @@
+require 'rubygems'
 require 'sqlite3'
 require_relative '../lib/student'
 
--- lib/student.rb
+++ lib/student.rb
@@ -2,18 +2,47 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+  end 
+
+  
+  def self.students_below_12th_grade
+  end 
+  
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+      
+      DB[:conn].execute(sql).map do |row|
+       self.new_from_db(row)
+  end
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
-  end
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE name = ?
+    LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+  end.first 
+  end 
   
   def save
     sql = <<-SQL
