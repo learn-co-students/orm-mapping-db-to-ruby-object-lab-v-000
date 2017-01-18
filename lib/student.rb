6a5e1289e4c1379fc9944a66c8965d86afe60138
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-01-18 13:35:01 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,7 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    self.new.tap { |student| student.id = row[0]; student.name = row[1]; student.grade = row[2]}
   end
 
   def self.all
@@ -13,17 +14,88 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = ? LIMIT 1
+    SQL
+
+    row = DB[:conn].execute(sql,name)[0]
+    row.nil? ? nil : self.new_from_db(row)
   end
-  
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade < 9
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade <= 11
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT * FROM students
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_x_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade <= 10 LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql,x).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade <= 10 LIMIT 1
+    SQL
+
+    row = DB[:conn].execute(sql)[0]
+    row.nil? ? nil : self.new_from_db(row)
+  end
+
+  def self.all_students_in_grade_x(x)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade <= ?
+    SQL
+
+    DB[:conn].execute(sql,x).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
