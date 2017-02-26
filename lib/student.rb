38f2d8189ab849868eee6fd452fa2f0ab4161973
Kenneth Holmquist <madifana@yahoo.com>
2017-02-26 22:01:22 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,28 +2,79 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE name = ?
+    LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
-  
+
+  def self.all_students_in_grade_x(x, limit = 100)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = ?
+    LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, x, limit).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.count_all_students_in_grade_9
+    self.all_students_in_grade_x(9)
+  end
+
+  def self.students_below_12th_grade
+    below_12th = []
+    below_12th << self.all_students_in_grade_x(9)
+    below_12th << self.all_students_in_grade_x(10)
+    below_12th << self.all_students_in_grade_x(11)
+    below_12th.flatten
+  end
+
+  def self.first_x_students_in_grade_10(x)
+    self.all_students_in_grade_x(10, x)
+  end
+
+  def self.first_student_in_grade_10
+    self.all_students_in_grade_x(10).first
+  end
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
