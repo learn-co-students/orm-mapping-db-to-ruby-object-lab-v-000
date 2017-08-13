d8d30958fdeeb53b526f1e5209495d34ad0bea1b
jonathan <jonathanyi81@gmail.com>
2017-08-12 21:13:20 -0400

    all method retrieves all records and createse an object from each row

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,14 @@ class Student
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      select *
+      from students
+    SQL
+
+    DB[:conn].execute(sql).map do |student|
+      self.new_from_db(student)
+    end
   end
 
   def self.find_by_name(name)
