76f0397b218b4b45b04b79e1cd1736da508475f6
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-25 22:11:02 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -8,20 +8,110 @@ class Student
     new_student.id = row[0]
     new_student.name =  row[1]
     new_student.grade = row[2]
-
     new_student
-
-    # create a new Student object given a row from the database
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    self.all.each do |student|
+      if student.name == name
+       return student
+      end
+    end
+  end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+
+    new_array = []
+
+    DB[:conn].execute(sql).map do |row|
+      new_array << self.new_from_db(row)
+    end
+
+    new_array.each do |student|
+      if student.grade != "9"
+        new_array.pop(student.id)
+      end
+    end
+
+    new_array
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+    SQL
+
+    new_array = []
+
+    DB[:conn].execute(sql).map do |row|
+      new_array << self.new_from_db(row)
+    end
+    new_array
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE id <= #{x}
+    SQL
+
+    new_array = []
+
+    DB[:conn].execute(sql).map do |row|
+      new_array << self.new_from_db(row)
+    end
+
+    new_array
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+
+    new_array = []
+
+    DB[:conn].execute(sql).map do |row|
+      new_array << self.new_from_db(row)
+
+    end
+    new_array.flatten[0]
+  end
+
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = #{x}
+    SQL
+
+    new_array = []
+
+    DB[:conn].execute(sql).map do |row|
+      new_array << self.new_from_db(row)
+    end
+    new_array
   end
 
   def save
