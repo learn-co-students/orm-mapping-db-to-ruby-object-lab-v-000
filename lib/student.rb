aa45781eaeefd1d832b989af608b91981d01f7d7
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-25 21:16:08 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,16 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+
+    new_student
+
     # create a new Student object given a row from the database
   end
 
@@ -14,16 +23,16 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
