9ae14f0a7e844e3c25ea37e4f0d6dcc7bb50c292
github username <github email address>
2017-10-11 12:03:24 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,7 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
+    # create a new Student object given a row from the database
     student = self.new
     student.id = row[0]
     student.name = row[1]
@@ -10,8 +11,11 @@ class Student
   end
 
   def self.all
+    # retrieve all the rows from the "Students" database
+    # remember each row should be a new instance of the Student class
     sql = <<-SQL
-      SELECT * FROM students
+      SELECT *
+      FROM students
     SQL
 
     DB[:conn].execute(sql).map do |row|
@@ -21,30 +25,45 @@ class Student
 
   def self.count_all_students_in_grade_9
     sql = <<-SQL
-      SELECT COUNT(*) FROM students WHERE grade=9
+      SELECT COUNT(*)
+      FROM students
+      WHERE grade=9
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.students_below_12th_grade
     sql = <<-SQL
-      SELECT name FROM students WHERE grade<12
+      SELECT *
+      FROM students
+      WHERE grade<12
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.first_X_students_in_grade_10(num)
     sql = <<-SQL
-      SELECT name FROM students WHERE grade=10
+      SELECT *
+      FROM students
+      WHERE grade=10
       LIMIT ?
     SQL
-    DB[:conn].execute(sql, num)
+    DB[:conn].execute(sql, num).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.first_student_in_grade_10
     sql = <<-SQL
-      SELECT * FROM students WHERE grade=10
+      SELECT *
+      FROM students
+      WHERE grade=10
+      ORDER BY students.id
       LIMIT 1
     SQL
     DB[:conn].execute(sql).map do |row|
@@ -54,20 +73,26 @@ class Student
   end
 
   def self.all_students_in_grade_X(grade)
-    sql = <<-SQL
-      SELECT * FROM students WHERE grade=?
-    SQL
-    DB[:conn].execute(sql, grade)
     # This is a class method that takes in an argument of grade for which to retrieve the roster. This method should return
     # an array of all students for grade X.
-
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade=?
+      ORDER BY students.id
+    SQL
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
     sql = <<-SQL
-      SELECT * FROM students WHERE name = ?
+      SELECT *
+      FROM students
+      WHERE name = ?
       LIMIT 1
     SQL
 
