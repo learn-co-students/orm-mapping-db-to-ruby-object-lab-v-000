df1f87e648964ee7c21e70046d73bfcf07fc30f6
GusVilla303 <gusvilla303@gmail.com>
2017-10-10 23:54:11 -0600

    Add 'LIMIT 1' to .find_by_name in Student

--- lib/student.rb
+++ lib/student.rb
@@ -25,6 +25,7 @@ class Student
           SELECT *
           FROM students
           WHERE name = ?
+          LIMIT 1
           SQL
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
