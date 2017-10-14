ee56809123f447fe4bcda2a18bce5d686af03590
Omar Donnadieu Mercado <donnadieu.86@gmail.com>
2017-10-14 16:55:31 +0000

    Done

--- lib/student.rb
+++ lib/student.rb
@@ -93,4 +93,31 @@ class Student
       self.new_from_db(row)
     end
   end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+      ORDER BY students.id
+    SQL
+
+    DB[:conn].execute(sql,grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
 end
