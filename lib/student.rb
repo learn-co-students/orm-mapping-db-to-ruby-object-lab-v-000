3bb334cd69eeaa5e4275da264336cde7044df8a0
Denis Bondin <dsbondin@gmail.com>
2017-10-22 18:42:57 +0000

    first X students

--- lib/student.rb
+++ lib/student.rb
@@ -46,4 +46,30 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT COUNT(*)
+      FROM students
+      WHERE grade = 9;
+    SQL
+
+    DB[:conn].execute(sql)[0]
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT name
+      FROM students
+      WHERE grade < 12;
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL
+    SQL
+  end
+
 end
