99ac01a9d7cc5437b3fbc95f15d3a424431b7eb8
zolkocarbon <eathena@hotmail.com>
2017-10-30 15:22:41 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -70,7 +70,7 @@ class Student
      SELECT *
      FROM students
      WHERE grade = 10
-     LIMIT 1
+     ORDER BY students.id LIMIT 1
      SQL
      DB[:conn].execute(sql).map do |row|
        self.new_from_db(row)
