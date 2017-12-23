58da8ef6f995c7a7e8d0909987068afd475637a6
Shibani Mookerjee <shibani.mookerjee@gmail.com>
2017-12-23 01:14:21 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -21,6 +21,45 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = "SELECT * FROM students WHERE name = (?) LIMIT 1;"
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.count_all_students_in_grade_9
+    sql = "SELECT * FROM students WHERE grade = '9'"
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.students_below_12th_grade
+    sql = "SELECT * FROM students WHERE grade <= '11'"
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_X_students_in_grade_10(limit)
+    sql = "SELECT * FROM students WHERE grade = '10' LIMIT (?)"
+    DB[:conn].execute(sql, limit).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    sql = "SELECT * FROM students WHERE grade = '10' LIMIT 1"
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = "SELECT * FROM students WHERE grade = ?"
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def save
