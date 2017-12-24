b55c77b5b59d775f05436a02eb49a25b1a1159c4
Christopher DiStasio <github email address>
2017-12-23 23:06:47 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -6,27 +6,22 @@ class Student
   @@all = []
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
-    new_student = self.new  # self.new is the same as running Song.new
+    new_student = self.new
     new_student.id = row[0]
-    new_student.name =  row[1]
+    new_student.name = row[1]
     new_student.grade = row[2]
-    new_student  # return the newly created instance
+    new_student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
-#     sql = <<-SQL
-#       SELECT *
-#       FROM students
-#       SQL
-
-# \DB[:conn].execute(sql)
-    
-
-
-    @@all
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
@@ -88,6 +83,33 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+    SELECT name
+    FROM students
+    WHERE grade = 10
+    SQL
+    DB[:conn].execute(sql).first(x)
+  end 
 
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT id, name
+    FROM students
+    WHERE grade = 10 
+    SQL
+    DB[:conn].execute(sql)[0][1]
+  end
 
-end
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    SQL
+    DB[:conn].execute(sql)[2][2]
+      if grade = DB[:conn].execute(sql)[2][2]
+        DB[:conn].execute(sql)
+      end
+  end 
+end      
+
