1961b193547aaf9cc4673b656c1b4b6c7745a8a6
Sherry El Garhy <sherryptk@gmail.com>
2017-12-28 21:27:30 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,17 +1,10 @@
 require 'pry'
 class Student
   attr_accessor :id, :name, :grade
-  attr_reader :id
-
-  # def initialize(name, grade, id = nil)
-  #   @name = name
-  #   @grade = grade
-  #   @id =id
-  # end
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    new_student = self.new  # self.new is the same as running Song.new
+    new_student = self.new  # self.new is the same as running Student.new
     new_student.id = row[0]
     new_student.name =  row[1]
     new_student.grade = row[2]
@@ -113,8 +106,19 @@ class Student
       LIMIT 1
     SQL
 
-    DB[:conn].execute(sql)
+    row = DB[:conn].execute(sql)
 
+    self.new_from_db(row[0])
+  end
+
+  def self.all_students_in_grade_X (grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = (?)
+    SQL
+
+    DB[:conn].execute(sql, grade)
   end
 
 end
