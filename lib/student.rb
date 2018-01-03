34693710435768cd798a9273a31d5a20e924ee9b
Sally Zasloff <szasloff@gmail.com>
2018-01-03 19:23:28 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -79,4 +79,31 @@ class Student
     end
   end
 
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT COUNT(*)
+      FROM students
+      WHERE grade < 12
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_X_students_in_grade_10(number)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, number).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+
+
 end
