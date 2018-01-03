289b2395ae19d3becba6d7c38c49435fc6091be3
Sally Zasloff <szasloff@gmail.com>
2018-01-03 19:28:47 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -117,4 +117,20 @@ class Student
     end.first
   end
 
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+
+
+
 end
