bb8615dbd7ee4316e7f7995ac48080432c1483a0
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-01-30 23:47:32 -0500

    Rspeclemented Student.new_from_db

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,13 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = self.new
+
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+
+    student
   end
 
   def self.all
