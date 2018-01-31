4c9c76b187c2e633fca28fd611a756f0dbe78833
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-01-31 00:56:32 -0500

    Rspeclemented Student.all_students_in_grade_X

--- lib/student.rb
+++ lib/student.rb
@@ -81,6 +81,18 @@ class Student
     end.first
   end
 
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
