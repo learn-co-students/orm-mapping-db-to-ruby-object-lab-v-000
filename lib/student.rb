97752213cebf88510e7fa6f314224e83c5aae769
Tyler Pulse <tyler3321@gmail.com>
2018-02-19 21:50:18 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -39,9 +39,9 @@ class Student
   
   def self.all_students_in_grade_X(grade)
     sql = <<-SQL
-    SELECT *
-    from Students
-    WHERE grade = ?
+      SELECT *
+      from Students
+      WHERE grade = ?
     SQL
     DB[:conn].execute(sql, grade).map do |row|
       self.new_from_db(row)
