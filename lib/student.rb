ec66322c6048dd729b950f427c71cf56a158746f
ElleeB <laura.trager@gmail.com>
2018-02-23 12:33:13 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,27 +3,51 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    all_array = DB[:conn].execute("SELECT * FROM students")
+    # each row becomes a new instance of the Student class
+    all_array.map do |student|
+      self.new_from_db(student)
+    end
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    # finds the student in the database given a name & returns a new instance of the Student class
+    row = DB[:conn].execute("SELECT * FROM students WHERE name = ? LIMIT 1", name).flatten
+    student = self.new_from_db(row)
+    student
   end
-  
+
+  # def self.find_by_name(name)
+  #   sql = <<-SQL
+  #     SELECT *
+  #     FROM songs
+  #     WHERE name = ?
+  #     LIMIT 1
+  #   SQL
+  #
+  #   DB[:conn].execute(sql, name).map do |row|
+  #     self.new_from_db(row)
+  #   end.first
+  # end
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
@@ -40,4 +64,45 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT COUNT(grade) FROM students
+      WHERE grade = 9;
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    DB[:conn].execute("SELECT * FROM students WHERE grade < 12;")
+  end
+
+  def self.first_X_students_in_grade_10(num_returned)
+    # returns an array of the first X students in grade 10
+    DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT ?;", num_returned)
+  end
+
+  def self.first_student_in_grade_10
+    # returns the first student in grade 10
+    self.all.find do |student_instance|
+      student_instance.grade == "10"
+    end
+  end
+
+  def self.all_students_in_grade_X(grade)
+    # returns an array of all students in a given grade X
+    DB[:conn].execute("SELECT * FROM students WHERE grade = ?;", grade)
+  end
+
+
+
+
+
+
+
+
+
+
+
+
 end
