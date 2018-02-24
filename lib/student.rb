c86ff14ea39ab4358da47daf8efdd33690ce6f37
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-02-24 15:05:37 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,17 +2,29 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = self.new
+    student.id, student.name, student.grade = row[0], row[1], row[2]
+    student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+    SQL
+    
+    table = DB[:conn].execute(sql)
+    students = table.collect {|row| new_from_db(row)}
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students WHERE name == (?)
+    SQL
+    
+    row = DB[:conn].execute(sql, name)
+    
+    student = new_from_db(row[0])
+    student
   end
   
   def save
@@ -39,5 +51,26 @@ class Student
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
+  end
+  
+  def self.count_all_students_in_grade_9
+    all.select {|student| student.grade == "9"}
+  end
+  
+  def self.students_below_12th_grade
+    all.select {|student| student.grade.to_i < 12}
+  end
+  
+  def self.first_X_students_in_grade_10(x)
+    all.select.first(x) {|student| student.grade == "10"}
+  end
+  
+  def self.first_student_in_grade_10
+    tenth_grade = all.select {|student| student.grade == "10"}
+    tenth_grade.first
+  end
+  
+  def self.all_students_in_grade_X(x)
+    all.select {|student| student.grade.to_i == x}
   end
 end
