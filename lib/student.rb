228d6cacf7e6feb29eca503483792afa888ed3ab
“Alyssa <clairaly13@gmail.comgit config --global user.name clairaly>
2018-02-28 17:41:43 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -22,8 +22,6 @@ class Student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
     sql = <<-SQL
       SELECT * FROM students
       WHERE name = ?
