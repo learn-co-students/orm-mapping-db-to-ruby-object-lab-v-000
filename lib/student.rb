a5b4bb018390a7966950e549d06259b35e308dfc
Chris Henry <cdhenry@gmail.com>
2018-03-07 21:55:01 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -68,42 +68,61 @@ class Student
 
   def self.count_all_students_in_grade_9
     sql = <<-SQL
-      SELECT * FROM students WHERE grade = ?
-    SQL
+      SELECT COUNT(*)
+      FROM students
+      WHERE grade = 9;
+      SQL
 
-    DB[:conn].execute(sql, 9)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.students_below_12th_grade
     sql = <<-SQL
-      SELECT * FROM students WHERE grade < ?
+      SELECT *
+      FROM students
+      WHERE grade < 12
     SQL
 
-    DB[:conn].execute(sql, 12)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
-  def self.first_X_students_in_grade_10(x)
+  def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
-      SELECT * FROM students WHERE grade = ? LIMIT ?
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id
+      LIMIT ?
     SQL
 
-    DB[:conn].execute(sql, 10, x)
+    DB[:conn].execute(sql, number).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.first_student_in_grade_10
     sql = <<-SQL
-      SELECT * FROM students WHERE grade = ? LIMIT ?
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id
+      LIMIT 1
     SQL
 
-    self.find_by_name(DB[:conn].execute(sql, 10, 1).flatten[1])
+    self.find_by_name(DB[:conn].execute(sql).flatten[1])
   end
 
-  def self.all_students_in_grade_X(x)
+  def self.all_students_in_grade_X(grade)
     sql = <<-SQL
-      SELECT * FROM students WHERE grade = ?
+      SELECT * FROM students WHERE grade = ? ORDER BY students.id
     SQL
 
-    DB[:conn].execute(sql, x)
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
   end
-
 end
