2dd380bb73adbc1eb27189f9960a53992aa571b5
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-03-09 03:17:05 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -22,6 +22,18 @@ class Student
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
   end
+  #
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT COUNT(*)
+      FROM students
+      WHERE grade = 9;
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
 
   def self.find_by_name(name)
     sql = <<-SQL
@@ -63,4 +75,6 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+
 end
