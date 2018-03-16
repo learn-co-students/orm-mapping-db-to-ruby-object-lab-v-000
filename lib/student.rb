1253ef2bdbd1d93d4137caadd11720ab23e7fcf6
Emily Zhao <tong.zhao.92@gmail.com>
2018-03-16 20:17:18 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,13 @@
 class Student
   attr_accessor :id, :name, :grade
 
+
+
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = Student.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
   end
 
   def self.all
@@ -14,16 +19,16 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
