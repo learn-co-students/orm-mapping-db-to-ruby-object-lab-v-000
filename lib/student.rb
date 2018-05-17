5db5655a3e8e0edda46b1138996f0cb5f9597740
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-17 01:08:02 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -76,4 +76,69 @@ class Student
     self.new_from_db(row)
     end
   end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade < "12"
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+    end
+  end
+
+  def self.all
+    sql = <<-SQL
+    SELECT *
+    FROM students
+
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+    end
+  end
+
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = "10"
+    ORDER BY students.id
+    LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql,num).map do |row|
+    self.new_from_db(row)
+  end
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    ORDER BY id
+    LIMIT 1
+    SQL
+
+
+    DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+    end
+  end
+
+  def self.all_students_in_grade_X(year)
+    sql = <<-SQL
+    SELECT *
+    FROM students,
+    WHERE grade ?
+    SQL
+
+    DB[:conn].execute(sql,year).map do |row|
+    self.new_from_db(row)
+    end
+  end
 end
