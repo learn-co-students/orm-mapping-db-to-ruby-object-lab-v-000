7ff1e40a148d232fb085d7ea00a2042eb426bdee
Ron Sala <ron.sala@gmail.com>
2018-05-25 06:17:35 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,23 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
+    # binding.pry
     # create a new Student object given a row from the database
+    # data = DB[:conn].execute("SELECT * FROM students WHERE id = row")
+    @id = row[0]
+    @name = row[1]
+    @grade = row[2]
+    # student = Student.new(id, name, grade)
+    student
+  end
+
+  def initialize(id:, name:, grade:)
+    @id = id
+    @name = name
+    @grade = grade
   end
 
   def self.all
@@ -14,16 +29,16 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
