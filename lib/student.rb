d0de1cabb19a881a834b1a8940ad69505efcca1f
Christopher P. Dundon <chris.dundon@gmail.com>
2018-05-29 21:23:28 -0400

    probably 2/3 of the way through.  Making good progress.

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,11 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+		new_student = self.new
+		new_student.id = row[0]
+		new_student.name = row[1]
+		new_student.grade = row[2]
+		new_student
   end
 
   def self.all
@@ -13,8 +18,38 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+		
+		sql = <<-SQL
+			SELECT * 
+			FROM students 
+			WHERE name = ?
+			LIMIT 1
+		SQL
+
+		rtn = DB[:conn].execute(sql, name)
+
+		rtn.map do |row|
+			self.new_from_db(row)
+		end.first
   end
   
+	def self.count_all_students_in_grade_9
+		sql = <<-SQL
+			SELECT COUNT(*)
+			FROM students
+			WHERE grade >= ? AND
+				grade <= ?
+		SQL
+
+		q_data = DB[:conn].execute(sql, 9, 9)
+
+		rtn = q_data.collect do |row|
+			self.new_from_db(row)
+		end
+
+		rtn
+	end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
@@ -40,4 +75,6 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+	
 end
