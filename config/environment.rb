c25a74506cca59d5d0214875f1f40ef2d5833f53
Matt Farmer <mrfarmer777@gmail.com>
2018-06-11 02:05:27 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
 require 'sqlite3'
 require_relative '../lib/student'
 
-DB = {:conn => SQLite3::Database.new("db/students.db")}
+DB = {:conn => SQLite3::Database.new("db/students.db")} #DB hash that allows you to connect to the database throughout your program.
