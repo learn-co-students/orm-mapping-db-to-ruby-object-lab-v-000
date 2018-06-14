3ed464b0cfcbeed2854dedbdb4038fe4cad30bf4
sgibson47 <sgibson47@gmail.com>
2018-06-14 15:36:31 -0500

    add #self.count_all_students_in_grade_X

--- lib/student.rb
+++ lib/student.rb
@@ -108,4 +108,17 @@ class Student
       self.new_from_db(row)
     end
   end
+
+  def self.count_all_students_in_grade_X(num)
+    sql =<<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, num).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
 end
