4c40f7bbfa7115d828c332886a2cce7a5cd2091d
sgibson47 <sgibson47@gmail.com>
2018-06-14 15:19:07 -0500

    update #self.all to retrieve all the rows from the 'Students' database and iterate through resulting array to make each row an instance of Student class

--- lib/student.rb
+++ lib/student.rb
@@ -10,8 +10,14 @@ class Student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql =<<-SQL
+    SELECT *
+    FROM students
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
