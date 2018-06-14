18c6ba6ee5cebe2acecbe26505c63068c274a7ef
sgibson47 <sgibson47@gmail.com>
2018-06-14 15:32:23 -0500

    add .first_student_in_grade_10

--- lib/student.rb
+++ lib/student.rb
@@ -82,4 +82,17 @@ class Student
       self.new_from_db(row)
     end
   end 
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 end
