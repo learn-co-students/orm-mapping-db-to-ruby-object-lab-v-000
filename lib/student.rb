078e7ebb3a3e7e2bf04c5139ca7e57c4e4957c4d
sgibson47 <sgibson47@gmail.com>
2018-06-14 15:34:51 -0500

    add #self.first_X_students_in_grade_10

--- lib/student.rb
+++ lib/student.rb
@@ -91,8 +91,21 @@ class Student
     LIMIT 1
     SQL
 
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
+  end
+
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, num).map do |row|
+      self.new_from_db(row)
+    end
   end
 end
