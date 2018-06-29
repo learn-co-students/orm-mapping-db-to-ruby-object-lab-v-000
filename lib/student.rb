8958b99b5a7bcfd0e5d401736c73207aa6377d2b
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-06-29 00:53:35 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -33,30 +33,47 @@ class Student
     sql = <<-SQL
       SELECT * FROM students WHERE grade = 9
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.students_below_12th_grade
-    self.all.select {|s| s.grade.to_i < 12}
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
       SELECT * FROM students WHERE grade = 10 LIMIT ?
     SQL
-    DB[:conn].execute(sql, number)
+    DB[:conn].execute(sql, number).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.first_student_in_grade_10
-    self.all.detect {|s| s.grade.to_i == 10}
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10 ORDER BY students.id LIMIT 1
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def self.all_students_in_grade_X(grade)
     sql = <<-SQL
       SELECT * FROM students WHERE grade = ?
     SQL
-    DB[:conn].execute(sql, grade)
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
   end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
