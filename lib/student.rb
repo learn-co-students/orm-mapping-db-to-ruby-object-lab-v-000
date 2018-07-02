23a5ffb341753e78d46c9fa655215222a99920c7
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-02 16:22:44 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,23 @@
+# require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
+
+  def self.find_by_name(name)
+    
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
-  end
-
-  def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
   end
   
   def save
