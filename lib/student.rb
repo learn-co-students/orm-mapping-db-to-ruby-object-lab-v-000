41ecd8347f38e04e625fd248ad749931548e19db
Kevin Chi <kevinchi118@gmail.com>
2018-07-17 15:43:32 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -95,5 +95,28 @@ class Student
        end
    end
 
+   def self.first_student_in_grade_10
+       sql = <<-SQL
+            SELECT *
+            FROM students
+            WHERE students.grade = 10
+            LIMIT 1
+       SQL
 
+       DB[:conn].execute(sql).collect do |row|
+           self.new_from_db(row)
+       end.first
+   end
+
+   def self.all_students_in_grade_X(grade)
+       sql = <<-SQL
+            SELECT *
+            FROM students
+            WHERE students.grade = ?
+       SQL
+
+       DB[:conn].execute(sql, grade).collect do |row|
+        self.new_from_db(row)
+       end 
+   end
 end
