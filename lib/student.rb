44834039bdbe568a826911302fbadb560438515f
github username <github email address>
2018-07-30 10:57:03 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -71,7 +71,7 @@ class Student
 
     def self.students_below_12th_grade
       sql = <<-SQL
-      SELECT SUM(name) FROM students WHERE grade <= 11
+      SELECT * FROM students WHERE grade <= 11
       SQL
       DB[:conn].execute(sql)
    end
