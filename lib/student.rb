e1e7ceff538a0bf584157fb84476a1e9ff15331e
github username <github email address>
2018-07-30 10:50:29 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -40,4 +40,44 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.new_from_db(row)
+    new_student = self.new  # self.new is the same as running Song.new
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student  # return the newly created instance
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+ 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+    def self.count_all_students_in_grade_9
+       sql = <<-SQL
+       SELECT COUNT(name) FROM students WHERE grade = 9
+       SQL
+       DB[:conn].execute(sql)
+    end
+
+    def self.students_below_12th_grade
+      sql = <<-SQL
+      SELECT SUM(name) FROM students WHERE grade <= 11
+      SQL
+      DB[:conn].execute(sql)
+   end
+
+    def self.all
+
+    end
+
 end
