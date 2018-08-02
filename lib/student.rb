ec987f0004e5445bfe40d5cf40e5e044ba63fcd5
Brittany Tinnin <bntinnin7@gmail.com>
2018-08-02 20:53:46 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -10,6 +10,7 @@ class Student
     new_student
   end
 
+
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
@@ -21,6 +22,7 @@ class Student
       self.new_from_db(row)
     end
   end
+
 
   def self.find_by_name(name)
     # find the student in the database given a name
@@ -44,17 +46,44 @@ class Student
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
-  
+
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 9
+      SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade <= 11
+      SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
-
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
@@ -63,9 +92,9 @@ class Student
       grade TEXT
     )
     SQL
-
     DB[:conn].execute(sql)
   end
+
 
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
