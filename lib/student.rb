321e6f20a604ba67f12d24ef35c9f764ad491488
Leslie Stevens <flylady@mail.utexas.edu>
2018-08-21 16:47:19 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -23,9 +23,59 @@ class Student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE name = ?
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 9
+    SQL
+    array_ninth = DB[:conn].execute(sql)
+    array_ninth
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    where grade < 12
+    SQL
+    array_below_12th = DB[:conn].execute(sql)
+    array_below_12th
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    HAVING grade = 10
+    LIMIT x.to_int
+    SQL
+    array_tenth_limit = DB[:conn].execute(sql)
+    array_tenth_limit
+  end
+
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    GROUP BY grade
+    HAVING grade = x
+    SQL
+    array_in_grade = DB[:conn].execute(sql)
+    array_in_grade
+  end
+
 
   def save
     sql = <<-SQL
