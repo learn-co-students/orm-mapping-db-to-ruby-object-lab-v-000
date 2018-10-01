dfb1e260fc46f3e2f4f4f962d9ed5181cb72a070
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-01 16:50:51 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,17 +2,28 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = Student.new 
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student 
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+     sql = "SELECT * FROM students"
+    DB[:conn].execute(sql).map do |student_attributes|
+      new_from_db(student_attributes)
+    end 
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+   sql = "SELECT * FROM students WHERE name = ?"
+   result = DB[:conn].execute(sql,name)
+   new_from_db(result[0])
   end
   
   def save
@@ -40,4 +51,37 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+  
+  def self.all_students_in_grade_9
+    sql = "SELECT * FROM students WHERE grade = ?"
+    DB[:conn].execute(sql,9).map do |return_array|
+      new_from_db(return_array)
+    end 
+  end 
+  
+  def self.students_below_12th_grade 
+    sql = "SELECT * FROM students WHERE grade < ?"
+    DB[:conn].execute(sql,12).map do |return_array|
+      new_from_db(return_array)
+    end 
+  end 
+  
+  def self.first_X_students_in_grade_10(number)
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
+    DB[:conn].execute(sql,number).map do |return_array|
+      new_from_db(return_array)
+    end 
+  end 
+  
+  def self.first_student_in_grade_10 
+    first_X_students_in_grade_10(1)[0]
+  end 
+  
+  def self.all_students_in_grade_X(grade)
+    sql = "SELECT * FROM students WHERE grade = ?"
+    DB[:conn].execute(sql,grade).map do |return_array|
+      new_from_db(return_array)
+    end 
+  end 
+  
 end
