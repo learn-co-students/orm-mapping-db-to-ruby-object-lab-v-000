010e36da0db8586a4218d21e61769fd5760f26a7
George  Pianka <gpianka1@gmail.com>
2018-10-07 08:31:39 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,7 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  def self.count_all_students_in_grade_9
+  def self.all_students_in_grade_9
     sql = <<-SQL
       SELECT COUNT(*)
       FROM students
