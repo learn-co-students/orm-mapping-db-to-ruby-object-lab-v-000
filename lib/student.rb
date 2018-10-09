05b5a2a5eae034bc54a667806e826347f2d423eb
Laura Darlak <laura@polyporedesign.com>
2018-10-09 02:36:06 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -88,15 +88,16 @@ class Student
       end
   end
 
-  def self.first_X_students_in_grade_10(n)
+  def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
       SELECT *
       FROM students
       WHERE students.grade = 10
-      LIMIT 2
+      ORDER BY students.id
+      LIMIT ?
       SQL
 
-      DB[:conn].execute(sql).map do |row|
+      DB[:conn].execute(sql, number).map do |row|
         self.new_from_db(row)
       end
   end
@@ -106,21 +107,23 @@ class Student
       SELECT *
       FROM students
       WHERE students.grade = 10
+      ORDER BY students.id LIMIT 1
       SQL
 
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end.first # chaining, grab the first element frmo the returned array
+    end.first # chaining, grab the first element from the returned array
   end
 
   def self.all_students_in_grade_X(grade)
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE students.grade = grade
+      WHERE grade = ?
+      ORDER BY students.id
       SQL
 
-      DB[:conn].execute(sql).map do |row|
+      DB[:conn].execute(sql, grade).map do |row|
         self.new_from_db(row)
       end
   end
