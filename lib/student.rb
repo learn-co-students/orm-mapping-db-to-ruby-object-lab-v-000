e2229b264feaa3349a7cfb8698ea9623f797d21e
Samantha brake <sammy.brake@gmail.com>
2018-11-10 00:13:15 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Student
   attr_accessor :id, :name, :grade
 
@@ -60,6 +61,35 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 9
+    SQL
+    DB[:conn].execute(sql)
+
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade <= 11
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def first_X_students_in_grade_10
+    sql = <<-SQL
+    SQL
+  end
+
+  def first_student_in_grade_10
+  end
+
+  def all_students_in_grade_X(grade)
   end
 end
