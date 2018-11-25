0335350a404b3b28a5e6fdd86bfc30cf994f2b6f
Rafael Lopez <lpz.raf@gmail.com>
2018-11-25 17:48:53 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -112,4 +112,17 @@ class Student
     end
   end
   
+  def self.first_X_students_in_grade_10(number)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+ 
+    DB[:conn].execute(sql, number).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
 end
