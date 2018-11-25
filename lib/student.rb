ff2fc01907dff9f9dab1142fdb7faf5bc94226f5
Rafael Lopez <lpz.raf@gmail.com>
2018-11-25 17:33:01 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,9 @@ class Student
       FROM students
     SQL
  
-    DB[:conn].execute(sql)
+     DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
