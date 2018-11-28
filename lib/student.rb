781e15bd80d681d1aac05b67f0c5fa615efa94e1
Azhar Shah <azharshahstl@yahoo.com>
2018-11-28 00:51:25 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -40,8 +40,17 @@ class Student
     
     DB[:conn].execute(sql)
   end
-    
   
+  def self.students_below_12th_grade 
+    binding.pry
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade < 12
+    SQL
+    
+    DB[:conn].execute(sql)
+  end 
+    
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
