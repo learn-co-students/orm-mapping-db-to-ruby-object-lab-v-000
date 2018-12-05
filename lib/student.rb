ad1eb4a9f0d088193c8acb0965f3e3ec3db8093f
Steven Crouse <sdcrouse91@gmail.com>
2018-12-05 21:07:46 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,16 +3,37 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    
+    self.new.tap do |student|
+      student.id = row[0]
+      student.name = row[1]
+      student.grade = row[2]
+    end
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    
+    sql = "SELECT * FROM students"
+    
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    
+    sql = <<-SQL
+      SELECT * 
+      FROM students 
+      WHERE name = ?
+      LIMIT 1
+    SQL
+    
+    self.new_from_db( DB[:conn].execute(sql, name).first )
   end
   
   def save
@@ -39,5 +60,63 @@ class Student
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
+  end
+  
+  def self.all_students_in_grade_9
+    # This works as well: self.all.select {|student| student.grade == "9"}
+    
+    sql = "SELECT * FROM students WHERE grade = 9"
+    
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  def self.students_below_12th_grade
+    # Equivalently: self.all.select {|student| student.grade.to_i < 12}
+    
+    sql = "SELECT * FROM students WHERE grade < 12"
+    
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  def self.first_X_students_in_grade_10(number_of_students)
+    # Equivalently: 
+    # self.all.select {|student| student.grade == "10"}.slice(0, number_of_students)
+    
+    sql = <<-SQL
+      SELECT * 
+      FROM students 
+      WHERE grade = 10 
+      LIMIT ?
+    SQL
+    
+    DB[:conn].execute(sql, number_of_students).collect do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  def self.first_student_in_grade_10
+    # Equivalently: self.all.select {|student| student.grade == "10"}.first
+    # Or: self.first_X_students_in_grade_10(1).first
+    
+    sql = <<-SQL
+      SELECT * 
+      FROM students 
+      WHERE grade = 10 
+      LIMIT 1
+    SQL
+    
+    self.new_from_db( DB[:conn].execute(sql).first )
+  end
+  
+  def self.all_students_in_grade_X(grade_X)
+    # Equivalently: self.all.select {|student| student.grade.to_i == grade_X}
+    
+    sql = "SELECT * FROM students WHERE grade = ?"
+    
+    DB[:conn].execute(sql, grade_X).collect {|row| self.new_from_db(row)}
   end
 end
