211ce93e3f03c89272f058e4f92b46c0de4b5276
taisaoko <ttd3calvin@gmail.com>
2018-12-09 14:51:13 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -79,10 +79,10 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10
-      
+      LIMIT ?
     SQL
  
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, num).map do |row|
       self.new_from_db(row)
     end
   end
