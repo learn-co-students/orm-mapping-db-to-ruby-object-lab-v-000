0a09da6eaca5979cdabc680502f12d198b972463
Elias Ventura <eliasventura23@gmail.com>
2019-01-18 21:53:01 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,14 @@
 class Student
-  attr_accessor :name, :grade, :id
+  attr_accessor :id, :name, :grade
+
 
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
-      grade TEXT
-    );
+      grade INTEGER
+    )
     SQL
 
     DB[:conn].execute(sql)
@@ -27,17 +28,16 @@ class Student
     VALUES (?, ?)
     SQL
 
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
     DB[:conn].execute(sql, self.name, self.grade)
-
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students;")[0][0]
   end
 
   def self.new_from_db(row)
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.find_by_name(name)
@@ -45,82 +45,73 @@ class Student
     SELECT *
     FROM students
     WHERE name = ?
-    LIMIT 1
     SQL
 
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first
+    row = DB[:conn].execute(sql, name)[0]
+    new_from_db(row)
   end
 
   def self.count_all_students_in_grade_9
-
     sql = <<-SQL
-    SELECT COUNT(*)
+    SELECT *
     FROM students
-    WHERE grade = "9"
+    WHERE grade = 9
     SQL
 
     DB[:conn].execute(sql)
   end
 
   def self.students_below_12th_grade
-
     sql = <<-SQL
     SELECT *
     FROM students
-    WHERE grade < "12"
+    WHERE grade < 12
     SQL
 
     DB[:conn].execute(sql)
   end
 
   def self.all
-
     sql = <<-SQL
     SELECT *
     FROM students
     SQL
 
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
+    table = DB[:conn].execute(sql)
+
+    table.map {|row|
+    new_from_db(row)}
   end
 
-  def self.first_X_students_in_grade_10(arg)
-
+  def self.first_X_students_in_grade_10(num)
     sql = <<-SQL
     SELECT *
     FROM students
-    WHERE grade = "10"
+    WHERE grade = 10
     LIMIT ?
     SQL
 
-    DB[:conn].execute(sql, arg)
-
+    DB[:conn].execute(sql, num)
   end
 
   def self.first_student_in_grade_10
-
     sql = <<-SQL
     SELECT *
     FROM students
-    WHERE grade = "10"
-    LIMIT 1
+    WHERE grade = 10
     SQL
 
-    self.new_from_db(DB[:conn].execute(sql).flatten)
+    row = DB[:conn].execute(sql).first
+    new_from_db(row)
   end
 
-  def self.all_students_in_grade_X(arg)
-
+  def self.all_students_in_grade_X(grade)
     sql = <<-SQL
     SELECT *
     FROM students
     WHERE grade = ?
     SQL
 
-    DB[:conn].execute(sql, arg)
-
+    DB[:conn].execute(sql, grade)
   end
 end
