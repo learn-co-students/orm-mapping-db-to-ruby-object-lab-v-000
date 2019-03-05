54b8ccfcd1242f0588be8e1cfb084a0854cb0cc5
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-05 01:18:34 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,10 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
   end
 
   def self.all
@@ -14,16 +18,16 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
