f9c9ce92a116043617068e69ea20f025a033a584
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-07 22:35:41 +0000

    WIP

--- lib/student.rb
+++ lib/student.rb
@@ -60,8 +60,26 @@ class Student
   end
 
   def self.students_below_12th_grade
-    sql = "SELECT * FROM students WHERE grade != 12"
-    DB[:conn].execute(sql)
+    sql = "SELECT * FROM students WHERE grade < 12"
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
+  def self.all
+    sql = "SELECT * FROM students"
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_X_students_in_grade_10(num)
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
+    DB[:conn].execute(sql, num)
+  end
+
+  def self.first_student_in_grade_10
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT 1"
+    DB[:conn].execute(sql)
+  end
 end
