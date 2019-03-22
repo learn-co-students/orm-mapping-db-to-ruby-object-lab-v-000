5fb01e1ebf34b05b04f26ba7b2670b58414b1c03
Reno Sorensen <renojames19@gmail.com>
2019-03-22 00:29:21 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -109,4 +109,16 @@ def self.first_student_in_grade_10
   end.first
 end
 
+def self.all_students_in_grade_X(num)
+  sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = ?
+  SQL
+
+  DB[:conn].execute(sql, num).map do |row|
+    self.new_from_db(row)
+  end
+end
+
 end
