ce990547715792ea7914fc6448b17be9ddb33b8d
Reno Sorensen <renojames19@gmail.com>
2019-03-22 00:24:44 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -71,4 +71,16 @@ def self.all_students_in_grade_9
   end
 end
 
+def self.students_below_12th_grade
+  sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade < 12
+  SQL
+
+  DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+  end
+end
+
 end
