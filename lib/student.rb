c24856dfba4f439deb67173bebd2858809d4a32f
Karly Mareka <karly.mareka@gmail.com>
2019-04-01 03:10:57 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,11 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = self.new 
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
