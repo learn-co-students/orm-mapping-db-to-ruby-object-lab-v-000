fa93c4201e79d4cdb0aa520a7a41177d7533811b
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-04-23 05:02:41 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
       SELECT * FROM students;
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map { |row| self.new_from_db(row) }
   end
 
   def self.find_by_name(name)
@@ -29,37 +29,45 @@ class Student
 
   def self.all_students_in_grade_9
     sql = <<-SQL
-
+      SELECT * FROM students WHERE grade = 9;
     SQL
+
+    DB[:conn].execute(sql)
 
   end
 
   def self.students_below_12th_grade
     sql = <<-SQL
-
+        SELECT * FROM students WHERE grade <= 11;
     SQL
 
+    DB[:conn].execute(sql).map { |row| self.new_from_db(row) }
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(num)
     sql = <<-SQL
-
+      SELECT * FROM students WHERE grade = 10 LIMIT #{num};
     SQL
 
+    DB[:conn].execute(sql).map { |row| self.new_from_db(row) }
   end
 
   def self.first_student_in_grade_10
     sql = <<-SQL
-
+      SELECT * FROM students WHERE grade = 10 LIMIT 1;
     SQL
 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
-  def self.all_students_in_grade_X
+  def self.all_students_in_grade_X(num)
     sql = <<-SQL
-
+      SELECT * FROM students WHERE grade = #{num};
     SQL
 
+    DB[:conn].execute(sql).map { |row| self.new_from_db(row) }
   end
 
 
