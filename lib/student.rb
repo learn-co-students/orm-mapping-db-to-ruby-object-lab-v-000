308d9b354e8c50bf66def07fa610a4fa961c8962
Karen Flores <karen.ag.flores@gmail.com>
2019-06-11 17:29:42 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -95,9 +95,10 @@ class Student
       WHERE grade = 10
     SQL
 
-    DB[:conn].execute(sql).map do |row|
+    array = DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
+    array[0...x]
   end
 
   def self.first_student_in_grade_10
