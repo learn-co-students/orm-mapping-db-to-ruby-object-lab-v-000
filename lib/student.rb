1b7c2c5f623b9d509142e400e90fb60966643088
xyn <xyn@Lonewolf.local>
2019-08-29 13:43:32 -0500

    refactored code repeatation with reify function

--- lib/student.rb
+++ lib/student.rb
@@ -1,73 +1,68 @@
 class Student
-  attr_accessor :id, :name, :grade
+    attr_accessor :id, :name, :grade
 
-  def self.new_from_db(row)
-    student = self.new
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
-    student
-  end
+    def self.new_from_db(row)
+        self.new.tap do |student|
+            student.id = row[0]
+            student.name = row[1]
+            student.grade = row[2]
+        end
+    end
 
-  def self.all
-    sql = "SELECT * FROM students"
-    DB[:conn].execute(sql).map { |row| self.new_from_db(row) }
-  end
+    def self.reify(db_data)
+        db_data.map { |row| self.new_from_db(row) }
+    end
 
-  def self.find_by_name(name)
-    sql = "SELECT * FROM students WHERE name = ?"
-    self.new_from_db(DB[:conn].execute(sql, name).first)
-  end
+    def self.all
+        self.reify(DB[:conn].execute("SELECT * FROM students"))
+    end
 
-  def self.count_all_students_in_grade_9
-    sql = "SELECT * FROM students WHERE grade = 9"
-    DB[:conn].execute(sql).map { |row| self.new_from_db(row) }
-  end
+    def self.find_by_name(name)
+        self.reify(DB[:conn].execute("SELECT * FROM students WHERE name = ?", name)).first
+    end
 
-  def self.students_below_12th_grade
-    sql = "SELECT * FROM students WHERE grade < 12"
-    DB[:conn].execute(sql).map { |row| self.new_from_db(row) }
-  end
+    def self.count_all_students_in_grade_9
+        self.reify(DB[:conn].execute("SELECT * FROM students WHERE grade = ?", 9))
+    end
 
-  def self.first_x_students_in_grade_10(x)
-    sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
-    DB[:conn].execute(sql, x).map { |row| self.new_from_db(row) }
-  end
+    def self.students_below_12th_grade
+        self.reify(DB[:conn].execute("SELECT * FROM students WHERE grade < ?", 12))
+    end
 
-  def self.first_student_in_grade_10
-    sql = "SELECT * FROM students WHERE grade = 10 ORDER BY id LIMIT 1"
-    self.new_from_db(DB[:conn].execute(sql).first)
-  end
+    def self.first_x_students_in_grade_10(x)
+        self.reify(DB[:conn].execute("SELECT * FROM students WHERE grade = ? LIMIT ?", 10, x))
+    end
 
-  def self.all_students_in_grade_x(x)
-    sql = "SELECT * FROM students WHERE grade = ?"
-    DB[:conn].execute(sql, x).map { |row| self.new_from_db(row) }
-  end
-  
-  def self.create_table
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade TEXT
-    )
-    SQL
+    def self.first_student_in_grade_10
+        self.reify(DB[:conn].execute("SELECT * FROM students WHERE grade = ? LIMIT ?", 10, 1)).first
+    end
 
-    DB[:conn].execute(sql)
-  end
+    def self.all_students_in_grade_x(x)
+        self.reify(DB[:conn].execute("SELECT * FROM students WHERE grade = ?", x))
+    end
+    
+    def self.create_table
+        sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS students (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            grade TEXT
+        )
+        SQL
 
-  def self.drop_table
-    sql = "DROP TABLE IF EXISTS students"
-    DB[:conn].execute(sql)
-  end
+        DB[:conn].execute(sql)
+    end
 
-  def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade) 
-      VALUES (?, ?)
-    SQL
+    def self.drop_table
+        DB[:conn].execute("DROP TABLE IF EXISTS students")
+    end
 
-    DB[:conn].execute(sql, self.name, self.grade)
-  end
+    def save
+        sql = <<-SQL
+            INSERT INTO students (name, grade) 
+            VALUES (?, ?)
+        SQL
 
+        DB[:conn].execute(sql, self.name, self.grade)
+    end
 end
