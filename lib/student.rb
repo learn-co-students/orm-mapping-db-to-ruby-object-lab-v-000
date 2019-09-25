5547a00cb7c63515b26e6870a4caa3d399cf0664
J Fern <jfern20@gmail.com>
2019-09-25 03:01:16 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -21,12 +21,77 @@ class Student
     self.new_from_db(row)
     end 
   end
+  
+  def self.students_below_12th_grade 
+    sql = <<-SQL 
+    SELECT * 
+    FROM students 
+    WHERE grade < 12; 
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+   end 
+  end 
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
     
-  end
+    sql = <<-SQL
+    SELECT * 
+    FROM students 
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+  end 
+   end 
+   
+   def self.first_X_students_in_grade_10(num)
+     sql = <<-SQL 
+     SELECT * 
+     FROM students 
+     WHERE grade = 10
+     ORDER BY students.id
+     LIMIT ? 
+     SQL
+     
+     DB[:conn].execute(sql, num).map do |row|
+       self.new_from_db(row)
+      
+   end 
+    end 
+    
+    def self.first_student_in_grade_10 
+      sql = <<-SQL 
+      SELECT *
+      FROM students
+      WHERE grade = 10 
+      ORDER BY students.id 
+      LIMIT 1
+      SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+      
+    end.first #will get first name 
+     end 
+     
+     
+     def self.all_students_in_grade_X(grade)
+       
+       sql = <<-SQL 
+       SELECT *
+       FROM students 
+       WHERE grade = ? 
+       ORDER BY students.id
+       SQL
+       
+       DB[:conn].execute(sql, grade).map do |row|
+         self.new_from_db(row)
+     end 
+   end 
+   
 
   def self.find_by_name(name)
     # find the student in the database given a name
