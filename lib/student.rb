5dd8109a6ca2ec5148bdc1ede887a0f502171bc4
Paris Dolfman <parisdolfman@gmail.com>
2020-06-25 19:15:47 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -40,6 +40,15 @@ class Student
     end
   end
 
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students WHERE NOT grade = 12
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
 
 
 
