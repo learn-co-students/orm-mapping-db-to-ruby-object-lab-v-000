f5ab4e0e0a3f3eeff79f705ce53e4ddbb4d880c3
Sai Petluri <spetluri.1@gmail.com>
2021-02-25 03:43:16 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -79,6 +79,7 @@ class Student
   end
   
   def self.first_student_in_grade_10
+    
     sql = <<-SQL
       select * 
       from students 
@@ -86,7 +87,7 @@ class Student
       limit 1;
       SQL
       
-    DB[:conn].execute(sql,name).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
 
