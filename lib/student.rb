49ec6801f9825219d6761eb301127a50cd22c6e6
Matthew Williams <matthew.williams@flatironschool.com>
2021-05-17 15:33:54 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,68 @@
 class Student
   attr_accessor :id, :name, :grade
 
+  def initialize(id=nil, name=nil, grade=nil)
+    @id = id
+    @name = name
+    @grade = grade
+  end 
+
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    self.new(row[0], row[1], row[2])
+  end
+  
+  def self.all_students_in_grade_9
+    sql = "SELECT * FROM students WHERE grade = ?"
+    students = DB[:conn].execute(sql, 9)
+    students.collect do |s|
+      self.new(s[0], s[1], s[2])
+    end
   end
 
+  def self.students_below_12th_grade
+    sql = "SELECT * FROM students WHERE grade < ?"
+    students = DB[:conn].execute(sql, 12)
+    students.collect do |s|
+      self.new(s[0], s[1], s[2])
+    end
+  end 
+
+  def self.first_X_students_in_grade_10(x)
+    sql = "SELECT * FROM students WHERE grade = ? LIMIT ?"
+    students = DB[:conn].execute(sql, 10, x)
+    students.collect do |s|
+      self.new(s[0], s[1], s[2])
+    end
+  end
+
+  def self.first_student_in_grade_10
+    sql = "SELECT * FROM students WHERE grade = ?"
+    students = DB[:conn].execute(sql, 10)
+    students.collect do |s|
+      self.new(s[0], s[1], s[2])
+    end.first
+  end 
+
+  def self.all_students_in_grade_X(x)
+    sql = "SELECT * FROM students WHERE grade = ?"
+    students = DB[:conn].execute(sql, x)
+    students.collect do |s|
+      self.new(s[0], s[1], s[2])
+    end
+  end 
+
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = "SELECT * FROM students"
+    students = DB[:conn].execute(sql)
+    students.collect do |s|
+      self.new(s[0], s[1], s[2])
+    end
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = "SELECT * FROM students WHERE name = ?"
+    student = DB[:conn].execute(sql, name).flatten
+    self.new(student[0], student[1], student[2])
   end
   
   def save
@@ -40,4 +90,5 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
 end
